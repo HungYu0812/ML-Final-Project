978d2183620c67f33c89c95690865d9969e88947
HungYu0812 <albein3402@gmail.com>
2021-01-01 20:00:44 +0800

    room type update

--- data_process_2.py
+++ data_process_2.py
@@ -11,8 +11,8 @@ meal_enc = preprocessing.OneHotEncoder()
 country_enc = preprocessing.OneHotEncoder()
 market_segment_enc = preprocessing.OneHotEncoder()
 distribution_channel_enc = preprocessing.OneHotEncoder()
-reserved_room_type_enc = preprocessing.OneHotEncoder()
-assigned_room_type_enc = preprocessing.OneHotEncoder()
+room_type_enc = preprocessing.OneHotEncoder()
+# assigned_room_type_enc = preprocessing.OneHotEncoder()
 deposit_type_enc = preprocessing.OneHotEncoder()
 agent_enc = preprocessing.OneHotEncoder()
 company_enc = preprocessing.OneHotEncoder()
@@ -20,16 +20,26 @@ customer_type_enc = preprocessing.OneHotEncoder()
 reservation_status_enc = preprocessing.OneHotEncoder()
 onehotDict = {'hotel': hotel_enc, 'meal': meal_enc, 'country': country_enc, 'market_segment': market_segment_enc,
               'distribution_channel': distribution_channel_enc,
-              'reserved_room_type': reserved_room_type_enc, 'assigned_room_type': assigned_room_type_enc, 'deposit_type': deposit_type_enc,
+              'reserved_room_type': room_type_enc, 'assigned_room_type': room_type_enc, 'deposit_type': deposit_type_enc,
               'agent': agent_enc, 'company': company_enc, 'customer_type': customer_type_enc, 'reservation_status': reservation_status_enc}
 for key in onehotDict:
-    tempArr = np.array(list(dictionary[key]))
+    if key == 'reserved_room_type' or key == 'assigned_room_type':
+        pass
+    tempList = list(dictionary[key])
+    tempList.sort()
+    tempArr = np.array(tempList)
     onehotArr = tempArr.reshape(-1, 1)
     onehotDict[key].fit(onehotArr)
-# print(onehotDict['meal'].categories_[0])
+room_type_list = list(dictionary['reserved_room_type'].union(
+    dictionary['assigned_room_type']))
+room_type_list.sort()
+room_type_forOneHot = np.array(room_type_list).reshape(-1, 1)
+room_type_enc.fit(room_type_forOneHot)
+
 arrival_date_month = {'January': 1, 'February': 2,
                       'March': 3, 'April': 4, 'May': 5, 'June': 6,
                       'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
+
 with open('train.csv', newline='') as csvfile:
     bigList = []
     newfield = np.array([])
@@ -71,10 +81,10 @@ with open('train.csv', newline='') as csvfile:
         bigList.append(arr)
         # f.write(','.join(arr))
         # f.write('\n')
-        # if count > 3:
-        #    break
+        if count > 500:
+            break
     # print(len(bigList[2]))
     print("Saving txt...")
-    np.savetxt('trainFTA.txt', np.array(bigList), fmt='%s', delimiter=',')
-    #np.savetxt('txtfile.txt', np.array(bigList), fmt='%s', delimiter=',')
+    # np.savetxt('trainFTA.txt', np.array(bigList), fmt='%s', delimiter=',')
+    np.savetxt('txtfile.txt', np.array(bigList), fmt='%s', delimiter=',')
     print("Done!")
